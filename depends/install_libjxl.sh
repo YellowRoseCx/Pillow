340b95df59c31798991b52599add37995f369bf6
YellowRoseCx <80486540+YellowRoseCx@users.noreply.github.com>
2026-01-03 16:17:28 -0600

    Optimize install_libjxl.sh
    Release Build & Strip Symbols
    Disable Extras
    Cleanup

--- depends/install_libjxl.sh
+++ depends/install_libjxl.sh
@@ -5,13 +5,17 @@ version=0.11.1
 ./download-and-extract.sh highway-1.3.0 https://github.com/google/highway/archive/1.3.0.tar.gz
 
 pushd highway-1.3.0
-cmake .
-make -j4 install
+cmake -DCMAKE_BUILD_TYPE=Release -DHWY_ENABLE_TESTS=OFF -DHWY_ENABLE_EXAMPLES=OFF .
+make -j4 install/strip
 popd
+
+rm -rf highway-1.3.0 highway-1.3.0.tar.gz
 
 ./download-and-extract.sh libjxl-$version https://github.com/libjxl/libjxl/archive/v$version.tar.gz
 
 pushd libjxl-$version
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DJPEGXL_ENABLE_SJPEG=OFF -DJPEGXL_ENABLE_SKCMS=OFF -DBUILD_TESTING=OFF .
-make -j4 install
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DJPEGXL_ENABLE_SJPEG=OFF -DJPEGXL_ENABLE_SKCMS=OFF -DBUILD_TESTING=OFF -DJPEGXL_ENABLE_BENCHMARK=OFF -DJPEGXL_ENABLE_EXAMPLES=OFF -DJPEGXL_ENABLE_MANPAGES=OFF .
+make -j4 install/strip
 popd
+
+rm -rf libjxl-$version libjxl-$version.tar.gz
